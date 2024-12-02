fc759e313e5882282a79b7fc097901ea61abe221
DESKTOP-TUNGNUI\ADMIN <viettri.tl9999@gmail.com>
2024-12-02 15:57:23 +0700

    add componets for app

---
+++ app/src/main/java/dev/amal/todo_compose/components/PriorityItem.kt
@@ -0,0 +1,37 @@
+package dev.amal.todo_compose.components
+
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import dev.amal.todo_compose.data.models.Priority
+import dev.amal.todo_compose.ui.theme.MEDIUM_PADDING
+import dev.amal.todo_compose.ui.theme.PRIORITY_INDICATOR_SIZE
+import dev.amal.todo_compose.ui.theme.Typography
+
+@Composable
+fun PriorityItem(priority: Priority) {
+    Row(verticalAlignment = Alignment.CenterVertically) {
+        Canvas(modifier = Modifier.size(PRIORITY_INDICATOR_SIZE)) {
+            drawCircle(color = priority.color)
+        }
+        Text(
+            modifier = Modifier.padding(start = MEDIUM_PADDING),
+            text = priority.name,
+            style = Typography.subtitle2,
+            color = MaterialTheme.colors.onSurface
+        )
+    }
+}
+
+@Composable
+@Preview
+fun PriorityItemPreview() {
+    PriorityItem(priority = Priority.LOW)
+}
