5529f20b7c0b728a56a53ef5c537f767486be33c
DESKTOP-TUNGNUI\ADMIN <viettri.tl9999@gmail.com>
2024-12-02 16:11:29 +0700

    add navigation

---
+++ app/src/main/java/dev/amal/todo_compose/navigation/Navigation.kt
@@ -0,0 +1,42 @@
+package dev.amal.todo_compose.navigation
+
+import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.navigation.NavHostController
+import com.google.accompanist.navigation.animation.AnimatedNavHost
+import dev.amal.todo_compose.navigation.destinations.listComposable
+import dev.amal.todo_compose.navigation.destinations.splashComposable
+import dev.amal.todo_compose.navigation.destinations.taskComposable
+import dev.amal.todo_compose.ui.viewmodels.SharedViewModel
+import dev.amal.todo_compose.utils.Constants.SPLASH_SCREEN
+
+@ExperimentalAnimationApi
+@ExperimentalMaterialApi
+@Composable
+fun SetupNavigation(
+    navController: NavHostController,
+    sharedViewModel: SharedViewModel
+) {
+    val screen = remember(navController) {
+        Screens(navController = navController)
+    }
+
+    AnimatedNavHost(
+        navController = navController,
+        startDestination = SPLASH_SCREEN
+    ) {
+        splashComposable(
+            navigateToListScreen = screen.splash
+        )
+        listComposable(
+            navigateToTaskScreen = screen.list,
+            sharedViewModel = sharedViewModel
+        )
+        taskComposable(
+            navigateToListScreen = screen.task,
+            sharedViewModel = sharedViewModel
+        )
+    }
+}
---
+++ app/src/main/java/dev/amal/todo_compose/navigation/destinations/TaskComposable.kt
@@ -0,0 +1,53 @@
+package dev.amal.todo_compose.navigation.destinations
+
+import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.slideInHorizontally
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.NavType
+import androidx.navigation.navArgument
+import com.google.accompanist.navigation.animation.composable
+import dev.amal.todo_compose.ui.screens.task.TaskScreen
+import dev.amal.todo_compose.ui.viewmodels.SharedViewModel
+import dev.amal.todo_compose.utils.Action
+import dev.amal.todo_compose.utils.Constants.TASK_ARGUMENT_KEY
+import dev.amal.todo_compose.utils.Constants.TASK_SCREEN
+
+@ExperimentalAnimationApi
+fun NavGraphBuilder.taskComposable(
+    sharedViewModel: SharedViewModel,
+    navigateToListScreen: (Action) -> Unit
+) {
+    composable(
+        route = TASK_SCREEN,
+        arguments = listOf(navArgument(TASK_ARGUMENT_KEY) {
+            type = NavType.IntType
+        }),
+        enterTransition = {
+            slideInHorizontally(
+                initialOffsetX = { -it },
+                animationSpec = tween(300)
+            )
+        }
+    ) { navBackStackEntry ->
+        val taskId = navBackStackEntry.arguments!!.getInt(TASK_ARGUMENT_KEY)
+        LaunchedEffect(key1 = taskId) {
+            sharedViewModel.getSelectedTask(taskId = taskId)
+        }
+        val selectedTask by sharedViewModel.selectedTask.collectAsState()
+
+        LaunchedEffect(key1 = selectedTask) {
+            if (selectedTask != null || taskId == -1)
+                sharedViewModel.updateTaskFields(selectedTask = selectedTask)
+        }
+
+        TaskScreen(
+            selectedTask = selectedTask,
+            sharedViewModel = sharedViewModel,
+            navigateToListScreen = navigateToListScreen
+        )
+    }
+}
