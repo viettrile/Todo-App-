22197cd8b7b1d0f5320073235b985f1de03935c6
DESKTOP-TUNGNUI\ADMIN <viettri.tl9999@gmail.com>
2024-12-02 16:14:19 +0700

    add MainActivity

---
+++ app/src/main/java/dev/amal/todo_compose/MainActivity.kt
@@ -0,0 +1,36 @@
+package dev.amal.todo_compose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.viewModels
+import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.navigation.NavHostController
+import com.google.accompanist.navigation.animation.rememberAnimatedNavController
+import dagger.hilt.android.AndroidEntryPoint
+import dev.amal.todo_compose.navigation.SetupNavigation
+import dev.amal.todo_compose.ui.theme.ToDoComposeTheme
+import dev.amal.todo_compose.ui.viewmodels.SharedViewModel
+
+@ExperimentalAnimationApi
+@ExperimentalMaterialApi
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+
+    private lateinit var navController: NavHostController
+    private val sharedViewModel: SharedViewModel by viewModels()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ToDoComposeTheme {
+                navController = rememberAnimatedNavController()
+                SetupNavigation(
+                    navController = navController,
+                    sharedViewModel = sharedViewModel
+                )
+            }
+        }
+    }
+}
